69b8069002ecf1c6b208d5e05768fb4affc24745
Gisele Tamie <giseletamie@hotmail.com>
2019-04-12 18:06:32 -0300

    Working layout v.4
    --Submenu screens:
    
    -Alterar limite de crédito;
    -Correção da gridline;
    -Adicionado ícone no app;
    -Tela resumo polida;
    -Incremento da tela de cliente.

--- app/src/main/java/com/FATEC/LES/ClienteActivity.java
+++ app/src/main/java/com/FATEC/LES/ClienteActivity.java
@@ -1,7 +1,13 @@
 package com.FATEC.LES;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class ClienteActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class ClienteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
+
+        FloatingActionButton btnAcao = (FloatingActionButton) findViewById(R.id.acbtnAcao);
+        btnAcao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LinearLayout linMenu = (LinearLayout) findViewById(R.id.linActionMenu);
+                if (linMenu.getVisibility() == View.VISIBLE){
+                    linMenu.setVisibility(View.GONE);
+                }else{
+                    linMenu.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+
+        Button btnAlterarLimite = (Button) findViewById(R.id.btnLimite);
+        btnAlterarLimite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inLimite = new Intent(ClienteActivity.this, LimCredActivity.class);
+                startActivity(inLimite);
+            }
+        });
     }
 }
