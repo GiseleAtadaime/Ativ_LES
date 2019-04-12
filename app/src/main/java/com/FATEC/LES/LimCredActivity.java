1cfdfd0f8317b9d507cc108bcf1480ef1dc86612
Gisele Tamie <giseletamie@hotmail.com>
2019-04-12 18:11:24 -0300

    Working layout v.4
    --Submenu screens:
    
    -Alterar limite de crédito;
    -Correção da gridline;
    -Adicionado ícone no app;
    -Tela resumo polida;
    -Incremento da tela de cliente.

---
+++ app/src/main/java/com/FATEC/LES/LimCredActivity.java
@@ -0,0 +1,23 @@
+package com.FATEC.LES;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LimCredActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lim_cred);
+
+        Button btnCancelar = (Button) findViewById(R.id.btnCancelar);
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LimCredActivity.super.onBackPressed();
+            }
+        });
+    }
+}
