563b180d69565b135b6f0b96b8b6817b929b936d
Gisele Tamie Atadaime <giseleatadaime@gmail.com>
2019-06-04 17:57:57 -0300

    CRUD v.1.3
    -MAJOR MODIFICATIONS:
    
    --Correção da tela de alterar limite
    --Adicionado DatePicker na pesquisa de duplicata
    --3 de 4 telas finalizadas
    
    -OTHERS:
    
    --Date picker class added
    --Data insert working
    --Data select working
    --Data update working

--- app/src/main/java/com/FATEC/LES/DuplicatasActivity.java
+++ app/src/main/java/com/FATEC/LES/DuplicatasActivity.java
@@ -1,7 +1,12 @@
 package com.FATEC.LES;
 
+import android.content.Context;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DuplicatasActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class DuplicatasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duplicatas);
+
+        TextView etxtData = findViewById(R.id.txtDatePicker);
+        etxtData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker(v);
+            }
+        });
+    }
+
+    public void showDatePicker(View v) {
+        DialogFragment newFragment = new MyDatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "date picker");
     }
 }
